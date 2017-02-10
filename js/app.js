d02bcab73dd6b83b478e7dc84e53552b96b497b8
Gurman Bhatia <gurmanbh@gmail.com>
2017-02-10 13:39:13 +0530

    somewhat functioning tooltip

--- js/app.js
+++ js/app.js
@@ -2,10 +2,22 @@ require('../css/styles.scss')
 var $ = require('jquery')
 var d3 = require('d3')
 var _ = require('underscore')
+var topojson = require('topojson')
+
+$("#all-spills-viz").append("<div class='tip'></div>");
+$(".tip").hide();
 
 var windowWidth = $(window).width(),
 windowHeight = 160,
 margin = {top: 30, bottom: 30, left: 40, right: 40}
+
+var projection = d3.geoMercator();
+var windowwidth = $(window).width();
+var pathf = d3.geoPath()
+    .projection(projection)
+    .pointRadius(2);
+var mapwidth = (windowwidth>450)?500:windowwidth*0.9;
+var mapheight = (windowwidth>450)?300:250
 
 windowWidth = (windowWidth>1200)?1200:windowWidth
 // scrolly for the kkhh nav
@@ -94,7 +106,7 @@ d3.csv('data/all-oil-spills.csv',function(error,data){
         .data(data)
         .enter()
         .append('rect')
-        .attr('class','spill')
+        .attr('class',function(d,i){return 'spill s-'+i })
         .attr('x', function(d) { return x(+d['year']) })
         .attr('y', function(d) { 
             var obj = _.findWhere(yearnest, {key:d['year']})
@@ -104,6 +116,8 @@ d3.csv('data/all-oil-spills.csv',function(error,data){
         .attr('height', blockheight)
         .style('fill',function(d){return getColor(d.quantity)})
         .on('click',function(e,i){console.log(e,i)})
+        .on("mouseover", tipOn)
+        .on("mouseout", tipOff);
 // x axis
     d3.selectAll('.chart-container-g')
             .append('g')
@@ -133,3 +147,86 @@ function getColor(e){
         return "#de2d26"
     }
 }
+
+// tooltips
+    svg.on("mouseout", function(){
+      $(".tip").hide()
+    });
+
+ function tipOn(d, i){
+
+      var dat = d;
+      console.log(d)
+        var elem = ".spill.s-" + i;
+      $(".tip").empty();
+
+      // show
+      $(".tip").show();
+      $(".spill").removeClass("highlight");
+      $(elem).addClass("highlight");
+      // populate
+      $(".tip").append("<div class='name'>" + dat.spilled_by + "</div>");
+      var blockheight = 10
+      // position
+      // calculate top
+      function calcTop(d){
+
+        var y = d.y;
+        var h = $(".tip").height();
+        var ot = $("svg").offset().top;
+        var st = $(window).scrollTop();
+        var r = blockheight;
+        var t = y - r - h + ot - st + 10;
+
+        if (t < 40){
+          t = y + r + (h/2.7) + ot - st + 10;
+          $(".tip").addClass("bottom").removeClass("top");
+        } else {
+          $(".tip").addClass("top").removeClass("bottom");
+        }
+
+        return t;
+      }
+
+      function calcLeft(d){
+        var x = d.x;
+        var r =blockheight;
+        var w = $(".tip").width();
+        var l = x - w / 2 + r;
+        return x + margin.left - w / 2;
+      }
+
+      $(".tip").css({
+        top: calcTop(dat),
+        left: calcLeft(dat)
+      });
+      $(window).resize(function(){
+        $(".tip").css({
+          top: calcTop(dat),
+          left: calcLeft(dat)
+        });
+      });
+
+    }
+
+    function tipOff(d){
+
+      $(".circle").removeClass("highlight");
+    }
+
+    d3.selection.prototype.moveToFront = function() {
+  return this.each(function(){
+    this.parentNode.appendChild(this);
+  });
+};
+d3.selection.prototype.moveToBack = function() {
+    return this.each(function() {
+        var firstChild = this.parentNode.firstChild;
+        if (firstChild) {
+            this.parentNode.insertBefore(this, firstChild);
+        }
+    });
+};
+
+// maps
+
